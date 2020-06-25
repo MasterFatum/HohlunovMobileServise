3c3c33a57709fce761f04acf118317a43774a497
Pi Volt <master_Fatum@hotmail.com>
2020-06-25 14:17:08 +0300

    Добавьте файлы проекта.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        public ActionResult Pays()
+        {
+            return View();
+        }
+
+        public ActionResult Delivery()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}
