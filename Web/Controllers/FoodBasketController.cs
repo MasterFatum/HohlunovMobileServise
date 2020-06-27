758def46643c6df13f935e8fe553ca6f35a65fc7
Pi Volt <master_Fatum@hotmail.com>
2020-06-27 12:56:33 +0300

    AddFoofBascetController, AddGoogleMaps

--- Web/Controllers/FoodBasketController.cs
+++ Web/Controllers/FoodBasketController.cs
@@ -6,30 +6,35 @@ using System.Web.Mvc;
 
 namespace Web.Controllers
 {
-    public class MainController : Controller
+    public class FoodBasketController : Controller
     {
-        //
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<FoodBasket> foodBaskets = context.FoodBasket.ToList().OrderBy(b => b.ProductName);
+
+            return View(foodBaskets);
         }
 
+        // GET: FoodBasket/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
+        // GET: FoodBasket/Create
         public ActionResult Create()
         {
             return View();
         }
 
+        // POST: FoodBasket/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
             try
             {
-                // TODO: Вставка данных в действие контроллера
+                // TODO: Add insert logic here
 
                 return RedirectToAction("Index");
             }
@@ -39,17 +44,19 @@ namespace Web.Controllers
             }
         }
 
+        // GET: FoodBasket/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
+        // POST: FoodBasket/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Вставка данных в действие контроллера
+                // TODO: Add update logic here
 
                 return RedirectToAction("Index");
             }
@@ -59,17 +66,19 @@ namespace Web.Controllers
             }
         }
 
+        // GET: FoodBasket/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
+        // POST: FoodBasket/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Вставка данных в действие контроллера
+                // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
             }
